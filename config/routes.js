8ebe3d49f72842066cae328e859d0a2613868678
Guilherme Abe <esx.gabe@terceiros.mag.com.br>
2022-11-13 16:57:28 -0300

    API NodeJS

--- config/routes.js
+++ config/routes.js
@@ -2,9 +2,9 @@ const express = require('express')
 const routes = express.Router()
  
 let db = [
-    { '1': { Nome: 'Cliente 1', Idade: '19' } },
-    { '2': { Nome: 'Cliente 2', Idade: '31' } },
-    { '3': { Nome: 'Cliente 3', Idade: '44' } },
+    { Id: '1', Nome: 'Cliente 1', Idade: '19' },
+    { Id: '2', Nome: 'Cliente 2', Idade: '31' },
+    { Id: '3', Nome: 'Cliente 3', Idade: '44', Profissao: 'Professor'}
 ]
 
 // Buscar Dados
@@ -13,7 +13,7 @@ routes.get('/', (req, res) => {
 })
 
 // Inserir Dados
-routes.post('/new', (req, res) => {
+routes.post('/add', (req, res) => {
     const body = req.body
 
     if(!body)
@@ -23,9 +23,9 @@ routes.post('/new', (req, res) => {
     return res.json(body)
 })   
 
-routes.delete('/:id', (req, res) => {
+routes.delete('/delete/:id', (req, res) => {
     const id = req.params.id
-
+    
     let newDB = db.filter(item => {
         if(!item[id])
             return item
@@ -35,9 +35,30 @@ routes.delete('/:id', (req, res) => {
 
     return res.send(newDB)
 })
-
-routes.put('/', (req, res) =>{
+routes.put('/put/:id', (req, res) =>{
     const id = req.params.id
+    const body = req.body
+
+    if(!body)
+        return res.status(400).end() 
+
+    const nome = body.Nome
+    const idade = body.Idade
+    var profissao = null
+    if(body.Profissao)
+        profissao = body.Profissao
+  
+
+    let person = db.find(p => {
+        if(p.Id === id){
+            p.Nome = nome
+            p.Idade = idade
+            p.Profissao = profissao
+            return p 
+        }
+    });
+
+    return res.send(person)
 })
 
 module.exports = routes
